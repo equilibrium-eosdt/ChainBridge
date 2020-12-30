fe85de7104d05f27cd8054121b0642e1348bbf8b
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2020-12-30 15:24:45 +0300

    Better scale amount (without precision lose)

--- chains/substrate/listener.go
+++ chains/substrate/listener.go
@@ -211,11 +211,10 @@ func (l *listener) handleEvents(evts utils.Events) {
 	if l.subscriptions[FungibleTransfer] != nil {
 		for _, evt := range evts.ChainBridge_FungibleTransfer {
 			l.log.Trace("Handling FungibleTransfer event")
-			amount := big.NewInt(int64(evt.Amount.Uint64()))
 			factor := big.NewInt(1000000000)
-			mul := big.NewInt(0)
-			mul = mul.Mul(amount, factor)
-			evt.Amount = types.NewU256(*mul)
+			amount := evt.Amount.Int
+		    amount = amount.Mul(amount, factor)
+			evt.Amount = types.NewU256(*amount)
 			l.submitMessage(l.subscriptions[FungibleTransfer](evt, l.log))
 		}
 	}
