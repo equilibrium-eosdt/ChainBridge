6945a1dbc36e0f3e8e6053a482b7ea55da5e5469
Vasily Kondratyev <wassily.kondratiev@gmail.com>
2021-03-12 14:16:47 +0300

    Rename Balances -> EqBalances in events

--- shared/substrate/events.go
+++ shared/substrate/events.go
@@ -149,9 +149,9 @@ type Events struct {
 	MultiAccount_MultisigExecuted          []EventMultisigExecuted                         // nolint:stylecheck,golint
 	MultiAccount_MultisigCancelled         []EventMultisigCancelled                        // nolint:stylecheck,golint
 	TreasuryReward_TreasuryMinting         []EventTreasuryMinting                          // nolint:stylecheck,golint
-	Balances_Transfer                      []equilibrium.EventBalancesTransfer             // nolint:stylecheck,golint
-	Balances_DeleteAccount                 []equilibrium.EventBalancesDeleteAccount        // nolint:stylecheck,golint
-	Balances_NotEnoughToBuyoutEq           []equilibrium.EventBalancesNotEnoughToBuyoutEq  // nolint:stylecheck,golint
+	EqBalances_Transfer                    []equilibrium.EventBalancesTransfer             // nolint:stylecheck,golint
+	EqBalances_DeleteAccount               []equilibrium.EventBalancesDeleteAccount        // nolint:stylecheck,golint
+	EqBalances_NotEnoughToBuyoutEq         []equilibrium.EventBalancesNotEnoughToBuyoutEq  // nolint:stylecheck,golint
 	Oracle_NewPrice                        []equilibrium.EventOracleNewPrice               // nolint:stylecheck,golint
 	Subaccounts_SubaccountCreated          []equilibrium.EventSubaccountsSubaccountCreated // nolint:stylecheck,golint
 	Subaccounts_RegisterBailsman           []equilibrium.EventSubaccountsRegisterBailsman  // nolint:stylecheck,golint
