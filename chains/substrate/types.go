509b4e82ed30b06a7f1a7a8be5e6bdb501625d3f
Maks Nabokov <max.nab@eosdt.com>
2021-06-02 20:01:55 +0300

    fix

--- chains/substrate/types.go
+++ chains/substrate/types.go
@@ -102,7 +102,7 @@ func (w *writer) createFungibleProposal(m msg.Message) (*proposal, error) {
 		method,
 		recipient,
 		amount,
-		types.NewBytes32(m.ResourceId)
+		types.NewBytes32(m.ResourceId),
 	)
 	if err != nil {
 		return nil, err
