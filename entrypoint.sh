1ab4289dc60e69204f863cd8ca05ea5c8f1c5a7b
Mikolaj Sobolewksi <meganuke@meganuke.ru>
2021-06-23 12:48:41 +0300

    Added third key

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,5 +28,6 @@ fi
 script -q -c 'usr/bin/bridge --config /etc/config.json --keystore /etc/keys --blockstore ${BLOCKSTORE} --graylog ${GRAYLOG} ${PARAMS} --metrics --metricsPort 9880' << ENDDOC
 1
 1
+1
 ENDDOC
 
